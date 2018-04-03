4fac9f502ecd125d7dc73bfd66b0169ab2280262
Siokhan <siokhank@gmail.com>
2018-04-03 21:35:53 +0100

    lists ingredients in csv with equivalent vector or each ingredient

--- BackEnd/organize.py
+++ BackEnd/organize.py
@@ -1,11 +1,18 @@
 import json
-import requests
+import pandas as pd 
+import numpy as np 
+from pandas import DataFrame
 
 masterList = []
 
-with open("input.json") as json_data:
+with open('input.json') as json_data:
     d = json.load(json_data)
-    # print(d['drinks'])
     for ingredient in d['drinks']:
         masterList.append(ingredient['strIngredient1'])
-    print(masterList)
+    #print(masterList)
+
+file = open('vectorKey.csv', 'w+')
+file.write('ingredient, vector\n')
+for index, i in enumerate(masterList):
+    print(i, index, end='\n')
+    file.write('{}, {}\n'.format(i, index))
