d3dd174e7c4d610d27fc14b9b8b32de9d331cc75
Siokhan <siokhank@gmail.com>
2018-04-13 17:23:58 +0100

    Got rid of csv ingredient vector key and changed all that functionality to json

--- BackEnd/organizeIngredients.py
+++ BackEnd/organizeIngredients.py
@@ -7,8 +7,13 @@ with open('input.json') as json_data:
     for ingredient in d['drinks']:
         masterList.append(ingredient['strIngredient1'])
 
-file = open('vectorKey.csv', 'w+')
-file.write('ingredient, vector\n')
-for index, i in enumerate(masterList):
-    print(i, index, end='\n')
-    file.write('{}, {}\n'.format(i, index))
+file = open('vectorKey.json', 'w+')
+cocks = []
+for index, name in enumerate(masterList):
+    print(name, index, end='\n')
+    cocks.append({
+        "index": index,
+        "name": name
+    })
+    # file.write('{}, {}\n'.format(i, index))
+json.dump(cocks, file)
--- BackEnd/updateIngredientList.sh
+++ BackEnd/updateIngredientList.sh
@@ -1,3 +1,3 @@
 rm input.json
 curl https://www.thecocktaildb.com/api/json/v1/1/list.php?i=list >> input.json
-python3 organize.py 
+python3 organizeIngredients.py 
