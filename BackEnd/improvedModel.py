e466fe8638399307f8dbc3edeb65d0c75826d868
Siokhan <siokhank@gmail.com>
2018-04-17 17:44:12 +0100

    classifier coming along closes #8 closes #9

--- BackEnd/improvedModel.py
+++ BackEnd/improvedModel.py
@@ -1,25 +1,24 @@
 from sklearn.feature_extraction.text import CountVectorizer
+from sklearn import svm
+from sklearn.naive_bayes import GaussianNB
 import json
 import numpy as np
+import pandas as pd
+import csv
 
 cocktailList = []
 corpus = []
 vectorizer = CountVectorizer()
 biVectorizer = CountVectorizer(ngram_range=(1, 2), token_pattern=r'\b\w+\b', min_df=1)
 
-# with open('cocktailList.json') as json_data:
-#     d = json.load(json_data)
-#     for cocktail in d:
-#         cocktailList.append(cocktail['ingredients'])
-
 with open('cocktailsVector.txt') as openfile:
     for line in openfile:
         x = line.split(' ')
-        print(vectorizer.fit_transform(x))
-        print(line)
+        #print(vectorizer.fit_transform(x))
+        #print(line)
         cocktailList.append({
-            "ingredients": line,
-            "label": 'valid'
+            'ingredients': line,
+            'label': 'valid'
         })
         corpus.append(line)
 
@@ -27,6 +26,29 @@ json.dump(cocktailList, open('labels.json', 'w+'))
             
 X2 = biVectorizer.fit_transform(corpus)
 shape = X2.shape
-print(X2)
+#print(X2)
 #print(corpus)
 #print(shape)
+
+data = pd.read_csv('cocktails.csv', encoding='ISO-8859-1')
+feature = ['ingredients']
+X = data.loc[:, feature]
+y = data.label
+print(y.shape)
+print(X)
+
+## classifier ##
+
+clf = svm.SVC()
+clf.fit(X2, y)
+
+gnb = GaussianNB()
+gnb.fit(X2.toarray(), y)
+
+test = ['11bc496ff310d1aae4df6ff0e8ff968a fc1cebf02dc2ee68655f3e7bf1b84230 9d3bce8eb5cec78e54d36dd0daf2bdc5 7d73dc0b6f7aeb20d99293fbdc5d6a6f 5ac0adcafdfad50d321f7cbcbe0060a0']
+badtest = ['0950210a45488b218b5ec9387827b69c 0950210a45488b218b5ec9387827b69c 0950210a45488b218b5ec9387827b69c 0950210a45488b218b5ec9387827b69c 0950210a45488b218b5ec9387827b69c 0950210a45488b218b5ec9387827b69c 0950210a45488b218b5ec9387827b69c 0950210a45488b218b5ec9387827b69c 0950210a45488b218b5ec9387827b69c']
+xtest = biVectorizer.transform(test)
+predicted = clf.predict(xtest)
+predicted2 = gnb.predict(test)
+print(predicted)
+print(predicted2)
--- BackEnd/organizeCocktail.py
+++ BackEnd/organizeCocktail.py
@@ -1,4 +1,7 @@
 import json
+import numpy as np
+import pandas as pd
+import csv
 
 cocktailList = []
 ingredientList = []
@@ -17,7 +20,7 @@ with open('cocktailList.json') as json_data:
 with open('vectorKey.json') as json_data:
     ingredientList = json.load(json_data)
 
-file = open('cocktailsVector.txt', 'w+')
+file = open('cocktailsHash.txt', 'w+')
 file2 = open('cocktails.txt', 'w+')
 for i, cocktail in enumerate(cocktailList):
     for ingredient in cocktail:
@@ -27,3 +30,20 @@ for i, cocktail in enumerate(cocktailList):
             file2.write(ingredient['name'] + ' ')
     file.write('\n')
     file2.write('\n')
+
+## Creating CSV file with cocktails and their attributes ##
+
+cocktailsParsed = json.load(open('labels.json'))
+cockData = open('cocktails.csv', 'w+')
+
+csvwriter = csv.writer(cockData)
+
+count = 0
+for cock in cocktailsParsed:
+    if count == 0:
+        header = cock.keys()
+        csvwriter.writerow(header)
+        count += 1
+    csvwriter.writerow(cock.values())
+
+cockData.close()
