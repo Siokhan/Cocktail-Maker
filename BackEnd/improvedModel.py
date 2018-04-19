5b90c800bb088db1bff2498c41db1bf54518762d
Siokhan <siokhank@gmail.com>
2018-04-20 00:12:27 +0100

    deleted useless files

--- BackEnd/improvedModel.py
+++ BackEnd/improvedModel.py
@@ -17,8 +17,6 @@ biVectorizer = CountVectorizer(ngram_range=(1, 2), token_pattern=r'\b\w+\b', min
 with open('cocktailsVector.txt') as openfile:
     for line in openfile:
         x = line.split(' ')
-        #print(vectorizer.fit_transform(x))
-        #print(line)
         cocktailList.append({
             'ingredients': line,
             'label': 'valid'
@@ -27,27 +25,25 @@ with open('cocktailsVector.txt') as openfile:
 
 json.dump(cocktailList, open('labels.json', 'w+'))   
 
-## Machine learning begin below ##            
+## Data for classifier organized ##            
 data = pd.read_csv('popularity.csv', encoding='ISO-8859-1')
-feature = ['ingredientsHashed']
+feature = 'ingredientsHashed'
 totalEntries = len(data.index)
 print(totalEntries)
-X = data.loc[:, feature]
-y = data.adjustedrating
 hashlist = []
-
 for i in range(0, totalEntries):
-    hashes = data.get_value(i, 'ingredientsHashed')
+    hashes = data.get_value(i, feature)
     hashlist.append(hashes)
-X3 = biVectorizer.fit_transform(hashlist)
+X = biVectorizer.fit_transform(hashlist)
+y = data.adjustedrating
 ## classifier ##
 
 clf = SGDClassifier(loss='hinge', penalty='l2', max_iter=5, shuffle=False)
-clf.fit(X3, y)
+clf.fit(X, y)
 joblib.dump(clf, 'classifier.pkl')
 
-test = ['916a17d53dff7bbd06635fac294e86eb e89b2cbb7d11825a67459af2249064de']
-badtest = ['3f3e574c181f45eea2aa2548e75f4434 909cea0c97058cfe2e3ea8d675cb08e1 99ba0855ce410fd0b68e2ceda2ffe98f e8f504860d2fbf92d26e69c85bc1486a 80766a792c83776a1302211303533d76']
+test = ['916a17d53dff7bbd06635fac294e86eb 4ca90a18e9f0d1242171c3c66074714b']
+badtest = ['3f3e574c181f45eea2aa2548e75f4434 909cea0c97058cfe2e3ea8d675cb08e1 80766a792c83776a1302211303533d76']
 xtest = biVectorizer.transform(test)
 ytest = biVectorizer.transform(badtest)
 predicted = clf.predict(xtest)
